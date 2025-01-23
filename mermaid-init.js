9b7a2b20eb201450116342227b945d308fcabb91
softtiny <yingxiancapatin@gmail.com>
2025-01-23 12:12:29 +0800

    :beers:

---
+++ mermaid-init.js
@@ -0,0 +1,16 @@
+// mdBook creates <code> elements with the class 'language-mermaid hljs' whenever you
+// define a mermaid code block.
+// The mermaid javascript parser looks for elements with a class name 'mermaid'.
+// So simply change the class name of the elements to 'mermaid' to make everything work.
+function patchMermaidCodeElementClass() {
+    var elements = document.getElementsByClassName("language-mermaid");
+    Array.from(elements).forEach(element => {
+        if (element.tagName.toLowerCase() == "code") {
+            element.className = "mermaid";
+        }
+    });
+    
+}
+
+patchMermaidCodeElementClass();
+mermaid.initialize({startOnLoad:true,theme:"forest"});
